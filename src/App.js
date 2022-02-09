f0d1c4556daad0051aae2bd1669f10ef0934efbf
WAHABKHADIM <wahabkhadim@gmail.com>
2022-02-09 19:41:12 +0500

    final file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { 
+  BrowserRouter,
+  Switch,
+  Route
+ }  from 'react-router-dom';
+import Login from './Components/Login';
+import Register from './Components/Register';
+import MyContext, { Provider } from './Components/StateContext';
+import MainPage from './Components/MainPage';
 
-function App() {
+
+
+
+class App extends React.Component {
+  constructor(props){
+    super(props)
+
+    this.state={
+      userList:[],
+      loginUser:null,
+      isLogin:false
+    }
+    this.onAddUser=this.onAddUser.bind(this)
+    this.checkLogin=this.checkLogin.bind(this)
+    this.checkSignOut=this.checkSignOut.bind(this)
+    this.handleLoginClick=this.handleLoginClick.bind(this)
+  }
+  static contextType =MyContext
+  onAddUser(obj){
+    const updatedlist = [...this.state.userList,obj ]
+
+    this.setState({
+        userList: updatedlist
+  })
+}
+handleLoginClick(item){
+  this.setState({loginUser:item})
+}
+
+
+checkLogin(){
+  this.setState({isLogin:true})
+}
+checkSignOut=()=>{
+  this.setState({
+    isLogin:false,
+    loginUser:null
+  })
+}
+
+
+  render(){
+    const data={
+      list:this.state.userList,
+      isLogin:this.state.isLogin,
+      loginUser:this.state.loginUser
+    }
+    console.log(data)
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Provider value={data}>
+      <BrowserRouter>
+      <Switch>
+      
+        <Route exact path="/"> <MainPage checkSignOut={this.checkSignOut} /> </Route>
+        <Route exact path="/register"> <Register onAddUser={this.onAddUser} /> </Route> 
+        <Route exact path="/login"> <Login checkLogin={this.checkLogin} handleLoginClick={this.handleLoginClick}  /> </Route>
+
+      
+        </Switch>
+      </BrowserRouter>
+      </Provider>
+
     </div>
   );
+}
 }
 
 export default App;
